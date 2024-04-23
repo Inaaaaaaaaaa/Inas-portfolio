bf5e0b55641474803d111efb4f47b149f9991596
Latte123456 <inayumi7@gmail.com>
2024-04-23 19:43:34 +1200

    Drop box for navigation bar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("home");
+    const [showSocials, setShowSocials] = useState(false); // Visibility of the socials dropdown
+
+    // Socials list
+    const socialList = [
+        { name: "LinkedIn", url: "https://www.linkedin.com/in/ina-youn-b7553a274/" },
+        { name: "Github", url: "https://github.com/Inaaaaaaaaaa" },
+    ];
+
     return (
-
-        //background of webpage 
         <div className="Background">
-            <h1>Welcome to Ina's portfolio!</h1>
+             <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")}>Home</li>
+                <li onClick={() => setMenu("aboutme")}>About me</li>
+                <li onClick={() => setMenu("projects")}>Projects</li>
+                <li onClick={() => {
+                    setShowSocials(!showSocials);
+                    setMenu("socials");
+                }} 
+                className={showSocials ? "no-underline" : ""}>
+                    Socials
+                    {showSocials && (
+                        <ul className="socials-dropdown">
+                            {socialList.map((social) => (
+                                <li key={social.name}>
+                                    <a href={social.url} target="_blank" rel="noopener noreferrer">{social.name}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </li>
+                <li onClick={() => setMenu("contact")}>Contact form</li>
+             </ul>
         </div>
-
-
     );
 };
 
