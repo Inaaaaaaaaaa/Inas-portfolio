6a5876b47ba8a08a1455083cdd012623f981ebbd
Latte123456 <inayumi7@gmail.com>
2024-05-02 11:05:20 +1200

    Updates
    - Background added and container colour changed
    
    - Input and text box have been moved

--- src/component/Contactme.jsx
+++ src/component/Contactme.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import './Contactme.css';
 
-export default function Contactme({contactRef}) {
-
+export default function Contactme({ contactRef }) {
     return (
-
         <div ref={contactRef} className="background">
             <div className="h1-top">
-            <h1>Contact me</h1>
+                <h1>Contact me</h1>
             </div>
 
-           <div className="container">
-                <input type="text" placeholder="First name " id="fname" required/>
-                <input type="text" placeholder="Last name " id="lname" required/>
-                <input type="text" placeholder="Email " id="email" required/>
-                <input type="text" placeholder="Phone number " id="phone" required/>
+            <div className="container">
+                <label htmlFor="fname">First name:</label>
+                <input type="text" placeholder="First name" id="fname" required/>
+
+                <label htmlFor="lname">Last name:</label>
+                <input type="text" placeholder="Last name" id="lname" required/>
+
+                <label htmlFor="email">Email:</label>
+                <input type="text" placeholder="Email" id="email" required/>
+
+                <label htmlFor="phone">Phone:</label>
+                <input type="text" placeholder="Phone number" id="phone" required/>
 
                 <div className="message-box">
-                <textarea placeholder="Message" id="message" required rows="4" cols="50"></textarea>
+                    <textarea placeholder="Message" id="message" required rows="4" cols="50"></textarea>
                 </div>
-                    
                 <button>Send</button>
             </div>
-
-
         </div>
-
-        
     );
-
 };
+
 
